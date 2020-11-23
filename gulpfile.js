085b279980db3a805d0f6e251a332bd373862dc0
AngelinaZhukovich <73842459+AngelinaZhukovich@users.noreply.github.com>
2020-11-23 20:01:36 +0300

    http://192.168.56.1:3000/

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,25 @@
 const gulp = require ('gulp');
 const sass = require ('gulp-sass');
 const rename = require ('gulp-rename');
-const browsersync = require ('browser-sync').create();
+const browserSync = require ('browser-sync').create();
+
+
+function styles() {
+    return gulp.src('src/scss/main.scss')
+        .pipe(sass())
+        .pipe(rename('styles.css'))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+           baseDir: "./",
+           index: "./index.html"
+        }
+    });
+    gulp.watch('src/scss/**/*.scss', styles)
+}
+
+exports.watch = watch;
