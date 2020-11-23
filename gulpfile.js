da4170ac15009eb29dca3d702c036a98666735a0
AngelinaZhukovich <73842459+AngelinaZhukovich@users.noreply.github.com>
2020-11-23 20:16:12 +0300

    cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require ('gulp');
 const sass = require ('gulp-sass');
+const cssnano = require ('gulp-cssnano');
 const rename = require ('gulp-rename');
 const browserSync = require ('browser-sync').create();
 
@@ -7,6 +8,7 @@ const browserSync = require ('browser-sync').create();
 function styles() {
     return gulp.src('src/scss/main.scss')
         .pipe(sass())
+        .pipe (cssnano())
         .pipe(rename('styles.css'))
         .pipe(gulp.dest('./'))
         .pipe(browserSync.stream());
