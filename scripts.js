76ceddf87cce976e8c0609df65adcfa62be65833
AngelinaZhukovich <73842459+AngelinaZhukovich@users.noreply.github.com>
2020-12-07 21:04:27 +0300

    js

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+// let ololol = "some text";
+// let ololol2 = `text $(ololol)`;   //строка
+// let num = 5; //числа
+// let bign = 3232323232323232; //большое число
+// let bool = true; //фолс - логич значение
+
+
+// let n = null;
+// let u = undefined; //не определено(переменная есть, но значения нет)
+// let nan =NaN; //неправ.математич запись not a namber
+// let inf = 6/0; //бесконечность
+// const //значение меняться не будет / объект
+// const car = {
+//     color: "red",
+//     wheel: 4
+// };
+
+// car.wheel 
+
+
+// const arr = [1,2,3,4,5,6] - //массив/ однотипные данные
+// arr[0]
+
+// // объект который можно вызвать
+function sum (a,b) {
+    let result = a+b;
+    console.log(result);
+    return result
+}
+
+sum(4,5);
+
+const newSum = (a,b) => a+b;
