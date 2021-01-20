3ad8a5061c0910197464a7b259b791fe9beca825
AngelinaZhukovich <73842459+AngelinaZhukovich@users.noreply.github.com>
2021-01-20 21:41:21 +0300

    add

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const { response } = require('express');
 const express = require('express');
 const app = express();
-const { Client } = require('pg')
-
+const { Client } = require('pg');
+const bodyParser = require ('body-parser');
 
 const client = new Client({
     connectionString: 'postgres://lnbsqlueizjals:bc14e6742886df26d18b37f0f511840b38b0d28f4973c25149110d661d9d01eb@ec2-34-252-98-12.eu-west-1.compute.amazonaws.com:5432/dd77nbv8psis3a',
@@ -20,6 +20,8 @@ const client = new Client({
 app.set('view engine', 'pug');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json())
+
 
 app.get('/', function(req, res){
     res.send('Hello');
@@ -50,4 +52,17 @@ app.listen(3000, function() {
 
 app.get ('/adduser', function(req, res){
   res.render('addUser');
-})
+});
+
+app.post('/createUser', function(req,res){
+  client.query(`INSERT 
+                INTO Persons (firstname, lastname) 
+                VALUES ('${req.body.firstname}', '${req.body.lastname}')`, (err, result)=>{
+                  if (err){
+                      res.status(500).json({error: err.stack})
+                  } else {
+                    res.status(200).json({response:result})
+                  }
+                })
+  
+});
--- public/js/script.js
+++ public/js/script.js
@@ -3,11 +3,36 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('#form');
     const btn = document.querySelector('.btn');
 
-    btn.addEventListener('click', function(e){
+
+    btn.addEventListener('click', function(e) {
         e.preventDefault();
-        console.log(form.elements);
+        let data = {};
+        const elements = [...form.elements];
+        for (let el of elements) {
+            if (el.type !== 'submit') {
+                data[el.name] = el.value;
+            }
+        }
+        console.log(data);
+        console.log(JSON.stringify(data));
+        fetch('/createUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => {
+            console.log(response);
+            return response.json();
 
-    })
-    
+            
+          })
+          .then((data) => {
+            console.log(data);
+            form.insertAdjacentHTML('beforeend', `<p class="alert alert-success">Insertrd </p>`)
 
+
+
+          });
+    });
 });
